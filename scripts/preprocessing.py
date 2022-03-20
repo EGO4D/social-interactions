9339ae032c2b98dcb1ae8a300c8c65d9d4a5e3ea
zcxu-eric <zhongcongxu@u.nus.edu>
2022-03-20 21:24:24 +0800

    s-

--- scripts/preprocessing.py
+++ scripts/preprocessing.py
@@ -1,16 +1,17 @@
+import os
 from get_json import get_json
 from get_lam_result import get_lam_result
 from get_ttm_result import get_ttm_result
 from merge import merge_json
 
 
-json_path = '../data/json/av.json'                # the path of annotation json file
-original_path = '../data/json_original'           # the saving path of annotation of the face bbox
-result_LAM_path = '../data/result_LAM'            # the saving path of LAM annotation
-result_TTM_path = '../data/result_TTM'            # the saving path of TTM annotation
+json_path = './data/json/av.json'                # the path of annotation json file
+original_path = './data/json_original'           # the saving path of annotation of the face bbox
+result_LAM_path = './data/result_LAM'            # the saving path of LAM annotation
+result_TTM_path = './data/result_TTM'            # the saving path of TTM annotation
 
 print("merge the json file...")
-merge_json('../data/json', json_path)
+merge_json('./data/json', json_path)
 
 print("processing tracklets...")
 get_json(json_path, original_path)
